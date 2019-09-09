0b584944673d46245df849ddab267c4976e0f0e6
hmashaw <hmashaw@gmail.com>
2019-09-09 12:13:47 -0400

    Create the library

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const createArrayOfSentences = text => {
+
+    // Find:
+    // ( -> )
+    // ^ -> . | ? | ! | $
+
+    var regex = /\(?[^\.\?!]+[\.\?!]+\)?|\(?[^\.\?!]+$/g
+    return text.match(regex)
+
+}
+
+const convertToLowerCase = arrayOfSentences => arrayOfSentences.map(sentence =>
+    sentence.toLowerCase().trim())
+
+const convertFirstCharToUpperCase = arrayOfSentences => arrayOfSentences.map(s =>
+    s.replace(/[a-z]/, c => c.toUpperCase()))
+
+const joinSentences = arrayOfSentences => arrayOfSentences.join(' ')
+
+/**
+ * Execute the reducer function on each array element
+ * reducer = (accumulator, currentFunc) => currentFunc(accumulator)
+ * 
+ * @param  {...function} funcs - a sequence of one parameter functions
+ * @return {function} A function that takes an inital value and runs it
+ *      through a sequence of functions, returning a single output value
+ * 
+ * @example
+ * 
+ *      pipe(func1, func2, func3)
+ *
+ */
+const pipe = (...funcs) => (initialValue) => funcs.reduce(reducer, initialValue)
+const reducer = (accumulator, currentFunc) => currentFunc(accumulator)
+
+// Create pipe and execute with initial value of text
+const convertParagraphToSentenceCase = text => pipe(
+    createArrayOfSentences,
+    convertToLowerCase,
+    convertFirstCharToUpperCase,
+    joinSentences
+)(text)
+
+// console.log(convertParagraphToSentenceCase(`THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG. IT BARKED.`))
+
+module.exports = { convertParagraphToSentenceCase }
